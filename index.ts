b2bd6ebf9f4366ffce3140cdd9a4ef3a639e5393
Olzhas <oljaskrutf@gmail.com>
2025-03-31 13:11:10 +0500

    easy bun use

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import { timestamp } from "./src/utils/utils"
+import { ariRoutes } from "./src/ari-routes"
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+app.use(express.json())
+
+app.use("/ari", ariRoutes)
+
+const startTime = timestamp()
+
+app.use(/^\/$/, (_, res) => {
+  res.send({ status: "OK", started: startTime, timestamp: timestamp() })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
--- start.js
+++
@@ -1,2 +0,0 @@
-await import("./index.ts")
-console.log("ARI-11 application started via start.js")
